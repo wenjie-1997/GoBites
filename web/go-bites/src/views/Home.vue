709270efd812420d7cee421184ac02fec97289fd
Mohd Ainal Farhan Mohamad Johari <56626447+Ainal-Farhan@users.noreply.github.com>
2020-11-05 11:54:57 +0800

    Initialize project

---
+++ web/go-bites/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="home">
+    <h1>Home</h1>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'Home'
+}
+</script>
+
+<style lang="scss" scoped>
+.home {
+    padding: 50px 5%;
+}
+</style>
