b67cafa47ebb7d039bbf9c3b5d5c549114509c8e
shibingong <73927018+shibingong@users.noreply.github.com>
2020-11-20 16:01:49 +0800

    add some pages

---
+++ demo/lib/pages/personalInfo.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/material.dart';
+
+class PersonalInfo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "View Personal Info",
+      home: PersonalInfoPage(),
+    );
+  }
+}
+
+class PersonalInfoPage extends StatefulWidget {
+  @override
+  _PersonalInfoPageState createState() => _PersonalInfoPageState();
+}
+
+class _PersonalInfoPageState extends State<PersonalInfoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.yellow[200],
+      appBar: AppBar(
+        title: Text('Personal Info'),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 0.0),
+        child: ListView(
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/default.png'),
+                radius: 50.0,
+              ),
+            ),
+            Divider(
+              height: 60.0,
+              color: Colors.red,
+            ),
+            Text(
+              'Username:',
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              'XXXXXXXXX',
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              'Password:',
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              'XXXXXXXXXX',
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              'Name:',
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              'XXX XXX XXX',
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              'Email Address:',
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              'XXXXXX@XXX.com',
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              'Gender:',
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              'XXX',
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              'Birthday:',
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              'XX-XX-XXXX',
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              'Address:',
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              'XXXXXXXXX',
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+       bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.red,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.yellow[200],
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person), label: 'Personal Info'),
+        ],
+        currentIndex: 1,
+      ),
+    );
+  }
+}
---
+++ demo/lib/pages/restMenupage.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class RestaurantMenuPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Restaurant Menu Page',
+      home: RestMenuPage(),
+    );
+  }
+}
+
+class RestMenuPage extends StatefulWidget {
+  @override
+  _RestMenuPageState createState() => _RestMenuPageState();
+}
+
+class _RestMenuPageState extends State<RestMenuPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.yellow[200],
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text('My Menu'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: 
+          EdgeInsets.fromLTRB(0, 10.0, 0, 10.0),
+        child: ListView(
+            children: <Widget>[
+              OutlineButton(
+                onPressed: () {},
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      flex: 1,
+                      child: CircleAvatar(
+                        backgroundImage:
+                            AssetImage('assets/default.png'),
+                        radius: 30.0,
+                      ),
+                    ),
+                    Expanded(
+                      flex: 2,
+                      child: Text('Spicy Chicken McDeluxe'),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Text('RM 20.00'),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+
+    );
+  }
+}
